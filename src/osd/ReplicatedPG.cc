a1a9e407cabc19a943de4458dd37d1a35eb847ad
Greg Farnum <greg@inktank.com>
2014-05-23 17:27:02 -0700

    ReplicatedPG: simplify getattrs_maybe_cache()
    Nobody calls getattrs_maybe_cache without specifying user_only, so remove
    the option and do filtering unconditionally. While we're there, remove the
    if(out) branches, because the out map is definitely required (the
    functions we call dereference it without checking).
    
    Signed-off-by: Greg Farnum <greg@inktank.com>

--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -3371,8 +3371,7 @@ int ReplicatedPG::do_osd_ops(OpContext *ctx, vector<OSDOp>& ops)
 	map<string, bufferlist> out;
 	result = getattrs_maybe_cache(
 	  ctx->obc,
-	  &out,
-	  true);
+	  &out);
         
         bufferlist bl;
         ::encode(out, bl);
@@ -5411,8 +5410,7 @@ int ReplicatedPG::fill_in_copy_get(
   if (!cursor.attr_complete) {
     result = getattrs_maybe_cache(
       ctx->obc,
-      &out_attrs,
-      true);
+      &out_attrs);
     if (result < 0) {
       if (cb) {
         delete cb;
@@ -12112,26 +12110,23 @@ int ReplicatedPG::getattr_maybe_cache(
 
 int ReplicatedPG::getattrs_maybe_cache(
   ObjectContextRef obc,
-  map<string, bufferlist> *out,
-  bool user_only)
+  map<string, bufferlist> *out)
 {
   int r = 0;
+  assert(out);
   if (pool.info.require_rollback()) {
-    if (out)
-      *out = obc->attr_cache;
+    *out = obc->attr_cache;
   } else {
     r = pgbackend->objects_get_attrs(obc->obs.oi.soid, out);
   }
-  if (out && user_only) {
-    map<string, bufferlist> tmp;
-    for (map<string, bufferlist>::iterator i = out->begin();
-	 i != out->end();
-	 ++i) {
-      if (i->first.size() > 1 && i->first[0] == '_')
-	tmp[i->first.substr(1, i->first.size())].claim(i->second);
-    }
-    tmp.swap(*out);
+  map<string, bufferlist> tmp;
+  for (map<string, bufferlist>::iterator i = out->begin();
+      i != out->end();
+      ++i) {
+    if (i->first.size() > 1 && i->first[0] == '_')
+      tmp[i->first.substr(1, i->first.size())].claim(i->second);
   }
+  tmp.swap(*out);
   return r;
 }
 
